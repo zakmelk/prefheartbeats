4265d2abb1ee5aacb3c3fa64ebf33e71d481485a
zakaria melk <zakariamelk@MacBook-Pro-de-zakaria.local>
2020-03-08 17:12:49 +0100

    Initial commit

---
+++ src/test/java/com/service/prefheartbeats/PrefheartbeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.prefheartbeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrefheartbeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
